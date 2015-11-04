e95abc0d773ae993d9910ff7f6713407f7d98f70
Joseph Abell <joseph.abell@piksel.com>
2015-11-04 11:55:42 +0000

    making the gulp actually use the server file from using require, rather than just slamming the server file directly into the gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var browserify = require('browserify'),
     source = require('vinyl-source-stream'),
     sourceFile = 'app/editor/public/js/main.js',
     destFolder = 'app/editor/public/js/',
-    destFile = 'cwine-editor.js';
+    destFile = 'cwine-editor.js',
+    server = require('./server.js');
+
 
 gulp.task('browserify', function() {
   return browserify(sourceFile, {debug:true})
@@ -27,31 +29,7 @@ gulp.task('watch', function() {
 });
 
 gulp.task('express', function () {
-  var express = require("express");
-  var app = express();
-  var path = __dirname + '/';
-  //var editor = require(path + "app/editor/js/editor.js");
-  // respond with "Hello World!" on the homepage
-  app.get('/', function (req, res) {
-    res.sendFile(path + 'app/game/views/index.html');
-  });
-
-  app.get('/edit', function (req, res) {
-    res.sendFile(path + 'app/editor/views/index.html');
-  });
-
-  app.use(express.static('app/shared/public'));
-  app.use(express.static('app/editor/public'));
-  app.use(express.static('app/game/public'));
-
-
-  app.use('*', function (req, res) {
-    res.sendFile(path + 'app/shared/views/error/404.html');
-  });
-
-  app.listen(8080,function(){
-    console.log("Live at Port 8080");
-  });
+  server.server();
 });
 
 gulp.task('default', ['browserify', 'watch', 'express']);
--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-var express = require("express");
-var app = express();
-var path = __dirname + '/';
-//var editor = require(path + "app/editor/js/editor.js");
-// respond with "Hello World!" on the homepage
-app.get('/', function (req, res) {
-  res.sendFile(path + 'app/game/views/index.html');
-});
+exports.server = function() {
+	var express = require("express");
+	var app = express();
+	var path = __dirname + '/';
+	//var editor = require(path + "app/editor/js/editor.js");
+	// respond with "Hello World!" on the homepage
+	app.get('/', function (req, res) {
+	  res.sendFile(path + 'app/game/views/index.html');
+	});
 
-app.get('/edit', function (req, res) {
-  res.sendFile(path + 'app/editor/views/index.html');
-});
+	app.get('/edit', function (req, res) {
+	  res.sendFile(path + 'app/editor/views/index.html');
+	});
 
-app.use(express.static('app/shared/public'));
-app.use(express.static('app/editor/public'));
-app.use(express.static('app/game/public'));
+	app.use(express.static('app/shared/public'));
+	app.use(express.static('app/editor/public'));
+	app.use(express.static('app/game/public'));
 
 
-app.use('*', function (req, res) {
-  res.sendFile(path + 'app/shared/views/error/404.html');
-});
+	app.use('*', function (req, res) {
+	  res.sendFile(path + 'app/shared/views/error/404.html');
+	});
 
-app.listen(8080,function(){
-  console.log("Live at Port 8080");
-});
+	app.listen(8080,function(){
+	  console.log("Live at Port 8080");
+	});	
+}
