b2be3b4c87884f328fab39288351a5cf90c8472c
bigjko <bigjko@gmail.com>
2015-12-11 00:14:38 +0100

    export changes

--- app/editor/public/js/exporter.js
+++ app/editor/public/js/exporter.js
@@ -4,8 +4,62 @@ const JSZip = require('jszip');
 const JSZipUtils = require('jszip-utils');
 const saveAs = require('filesaverjs').saveAs;
 
+const containsImage = function(image, list) {
+    var i;
+	let imagedata = image.split('base64,').pop()
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].data === imagedata) {
+            return list[i].name;
+        }
+    }
+
+    return undefined;
+};
+
+const handleImages = function(json) {
+	let images = [];
+	
+	for (let n=0; n<json.nodes.length; n++) {
+		let node = json.nodes[n];
+		if (node !== null && node.image !== undefined) {
+			let exists = containsImage(node.image, images);
+			if (exists !== undefined) {
+				node.image = 'img/' + exists;
+			} else {
+				let extension = '.jpeg';
+				if (node.image.indexOf('image/png') != -1) extension = '.png';
+				let newimage = { name: 'image_' + images.length + extension,
+								data: node.image.split('base64,').pop() };
+				images.push(newimage);
+				node.image = 'img/' + newimage.name;
+			}
+			if (node.elements !== undefined) {
+				for (let e=0; e < node.elements.length; e++) {
+					let element = node.elements[e];
+					if (element !== null && element.image !== undefined) {
+						let exists = containsImage(element.image, images);
+						if (exists !== undefined) {
+							element.image = 'img/' + exists;
+						} else {
+							let extension = '.jpeg';
+							if (element.image.indexOf('image/png') != -1) extension = '.png';
+							let newimage = { name: 'image_' + images.length + extension,
+											data: element.image.split('base64,').pop() };
+							images.push(newimage);
+							element.image = 'img/' + newimage.name;
+						}
+					}
+				}
+			}
+		}
+	}
+	return images;
+};
+
 exports.exportToZip = function(json) {
 	let zip = new JSZip();
+	let images = handleImages(json);
+	console.log(images);
 	//let done = 3;
 	
 	JSZipUtils.getBinaryContent('js/runtime-export.zip', function(err, data) {
@@ -13,7 +67,11 @@ exports.exportToZip = function(json) {
     	  throw err; // or handle the error
    		}
    		zip.load(data);
+		
    		zip.file('game.json', JSON.stringify(json, null, 4));
+		for (let i=0; i<images.length; i++) {
+			zip.file('img/'+images[i].name, images[i].data, {base64:true});
+		}
    		JSZipUtils.getBinaryContent('js/cwine-runtime.js', function(err, data) {
 			if(err) {
 	    	  throw err; // or handle the error
@@ -30,3 +88,4 @@ exports.exportToZip = function(json) {
 		});
    	});
 };
+
